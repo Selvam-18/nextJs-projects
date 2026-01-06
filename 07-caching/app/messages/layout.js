da094766e1bdba31d51418c67c1a33672a87a1e2
Selvam-18 <selvamk18.2025@gmail.com>
2026-01-06 23:29:21 +0530

    06/12

--- 07-caching/app/messages/layout.js
+++ 07-caching/app/messages/layout.js
@@ -1,10 +1,13 @@
-export default async function MessagesLayout({ children }) {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'layout',
-    },
-  });
-  const messages = await response.json();
+import { getMessages } from "@/lib/messages";
+import { unstable_noStore } from "next/cache";
+// export const revalidate = 5S
+// export const dynamic = 'force-dynamic'
+
+export default  function MessagesLayout({ children }) {
+  // const response = await fetch('http://localhost:8080/messages');
+  // unstable_noStore()
+  // const messages = await response.json();
+  const messages = getMessages()
   const totalMessages = messages.length;
 
   return (
